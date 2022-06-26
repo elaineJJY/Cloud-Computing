6a4cdb7a1113f051ffcecfde9473fd4748b32898
Jia, Jingyi <jingyi.jia@stud.tu-darmstadt.de>
2022-06-26 12:59:41 +0200

    "ex03 init"

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # Cloud-Computing
-Small exercises from course cloud computing
+Exercises from course cloud computing
+
+#### exercise-01
+
+Main Task: Writing a 
+
+Skills involed: Node.js
+
+<!--主要任务：-->
+
+<!--技术栈：Node.js-->
+
+#### exercise-02
+
+Main Task:
+
+Skills involed:
+
+<!--主要任务：-->
+
+<!--技术栈：-->
+
+#### exercise-03
+
+Main Task:
+
+Skills involed:
+
+<!--主要任务：-->
+
+<!--技术栈：-->
---
+++ exercise-03/product-descp-service/product_descp.js
@@ -0,0 +1,12 @@
+module.exports = function (options) {
+    //Import the mock data json file
+    const mockData = require('./MOCK_DATA.json');
+
+    //Add the patterns and their corresponding functions
+    this.add('role:product,cmd:getProductURL', productURL);
+    this.add('role:product,cmd:getProductName', productName);
+
+
+    //To DO: add the pattern functions and describe the logic inside the function
+
+}
